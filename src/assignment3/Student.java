4e6e7747d449e4107ee41cd5ad2d0187b55409fe
Marina <marinacarvalho@protonmail.com>
2024-02-12 09:59:40 -0500

    refactoring code and changing method's return

--- src/assignment3/Student.java
+++ src/assignment3/Student.java
@@ -2,25 +2,25 @@ package assignment3;
 
 import java.io.*;
 
-public class Student implements Serializable{
-	
+public class Student implements Serializable {
+
 	// declare variables
 	private int studentId;
 	private String name;
 	private double gpa;
-	
+
 	// no-arg constructor
 	public Student() {
-		
+
 	}
-	
+
 	// arg constructor
 	public Student(int studentId, String name, double gpa) {
 		this.studentId = studentId;
 		this.name = name;
 		this.gpa = gpa;
 	}
-	
+
 	// accessor and mutator for each variable
 	public int getStudentId() {
 		return studentId;
@@ -45,32 +45,32 @@ public class Student implements Serializable{
 	public void setGpa(double gpa) {
 		this.gpa = gpa;
 	}
-	
+
 	// method to convert the object to a byte array
-	public static byte[] toByteArray(Student obj) throws IOException{
+	public static byte[] toByteArray(Student obj) throws IOException {
 		ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
 		ObjectOutputStream objOut = new ObjectOutputStream(byteOut);
 		objOut.writeObject(obj);
 		return byteOut.toByteArray();
 	}
-	
+
 	// method to convert byte array to object
-	public static Object fromByteArray(byte[] data) throws IOException, ClassNotFoundException{
+	public static Object fromByteArray(byte[] data) throws IOException, ClassNotFoundException {
 		ByteArrayInputStream byteIn = new ByteArrayInputStream(data);
 		ObjectInputStream objIn = new ObjectInputStream(byteIn);
 		Object obj = objIn.readObject();
 		byteIn.close();
 		objIn.close();
 		// casting the return object
-		return (Student)obj;
+		return (Student) obj;
 	}
-	
+
 	// toString method printing information of object
 	@Override
 	public String toString() {
-		return "\nStudent" +
+		return "\nassignment3.Student" +
 				"\nId: " + getStudentId() +
 				"\nName: " + getName() +
-				"\nGPA: " + getGpa();	
+				"\nGPA: " + getGpa();
 	}
-} 
+}
